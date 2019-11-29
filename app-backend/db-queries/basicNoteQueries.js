a093ff2a809e3c95f2d021efdb69e97520d14fbc
jdc-cunningham <jcun4128@gmail.com>
2019-11-29 13:49:04 -0600

    back to working crud

--- app-backend/db-queries/basicNoteQueries.js
+++ app-backend/db-queries/basicNoteQueries.js
@@ -9,87 +9,31 @@ const createNote = (request, response) => {
     const dateTimeNow = utils.dateTimeNow();
 
     pool.query(
-        // 'INSERT INTO note_entries SET (`name`, `content`, `tags`, `created_at`, `updated_at`) VALUES ',
-        'INSERT INTO note_entries SET name=?,content=?,tags=?,created_at=?,updated_at=?',
+        'INSERT INTO note_entries SET name = ?, content = ?, tags = ?, created_at = ?, updated_at = ?',
         [name, content, tags, dateTimeNow, dateTimeNow],
-        (err, results, fields) => {
-            console.log(err);
-            console.log(results);
-            console.log(fields);
+        (error, results) => {
+            if (error) {
+                response.status(401).send('Failed to create new note'); // vague front end error
+            } else {
+                response.status(201).send(`Note created with ID: ${results.insertId}`);
+            }
         }
     );
-
-    // const prepSql = "INSERT INTO note_entries (`name`, `content`, `tags`, `created_at`, `updated_at`) VALUES (?, ?, ?, ?, ?)";
-    // const sqlInserts = [name, content, tags, dateTimeNow, dateTimeNow];
-    // const sql = mysql.format(prepSql, sqlInserts);
-    // pool.execute(sql, (err, results) => {
-    //     console.log(err);
-    //     console.log(results);
-    // });
-
-    // works
-    // pool.query(
-    //     'SELECT * FROM note_entries',
-    //     (err, results, fields) => {
-    //         console.log(err);
-    //         console.log(results);
-    //         console.log(fields);
-    //     } 
-    // );
-
-    // pool.query(
-    //     "INSERT INTO note_entries SET " +
-    //     "`name` = ?, `content` = ?, `tags` = ?, `created_at` = ?, `updated_at` = ?",
-    //     [
-    //         name,
-    //         content,
-    //         tags,
-    //         dateTimeNow,
-    //         dateTimeNow
-    //     ],
-    //     // 'INSERT INTO note_entries SET ' +
-    //     // '(name, content, tags, created_at, updated_at) VALUES ?' +
-    //     // '(?, ?, ?, ?, ?) VALUES ' +
-    //     // '(?, ?, ?, ?, ?) RETURNING id',
-    //     // 'name = :name, content = :content, tags = :tags, created_at = :created_at, updated_at = :updated_at',
-    //     // {
-    //     //     name,
-    //     //     content,
-    //     //     tags,
-    //     //     created_at: dateTimeNow,
-    //     //     updated_at: dateTimeNow
-    //     // },
-    //     'INSERT INTO note_entries (name, content, tags, created_at, updated_at) VALUES',
-    //     '?, ?, ?, ?, ?',
-    //     [name, content, tags, dateTimeNow, dateTimeNow],
-    //     (err, results) => {
-    //         console.log(err);
-    //         console.log(results);
-    //         // if (error) {
-    //         //     // need to handle these in a logger
-    //         //     console.log(error);
-    //         //     response.status(401).send('Failed to create new note'); // vague front end error
-    //         // } else {
-    //         //     response.status(201).send(`Note created with ID: ${JSON.stringify(results.rows[0].id)}`);
-    //         // }
-    //     }
-    // )
 };
 
 const getNote = (request, response) => {
     const noteId = request.body.noteId;
 
     pool.query(
-        'SELECT name, content FROM note_entries WHERE id = $1 ',
-        [
-            noteId
-        ],
-        (error, results) => {
+        'SELECT name, content FROM note_entries WHERE id = ?',
+        [noteId],
+        (error, results, fields) => {
+            console.log(results);
             if (error) {
                 // need to handle these in a logger
                 response.status(401).send('Failed to find note'); // vague front end error
             } else {
-                response.status(200).send(results.rows[0]);
+                response.status(200).send(results[0]);
             }
         }
     )
@@ -102,7 +46,7 @@ const updateNote = (request, response) => {
     const modified = utils.dateTimeNow();
 
     pool.query(
-        'UPDATE note_entries SET name = $1, content = $2, updated_at = $3 WHERE id = $4 ',
+        'UPDATE note_entries SET name = ?, content = ?, updated_at = ? WHERE id = ?',
         [
             noteName,
             noteContent,
@@ -124,7 +68,7 @@ const deleteNote = (request, response) => {
     const noteId = request.body.noteId;
 
     pool.query(
-        'DELETE FROM note_entries WHERE id = $1',
+        'DELETE FROM note_entries WHERE id = ?',
         [
             noteId
         ],
@@ -146,7 +90,7 @@ const searchNotes = (request, response) => {
     const partialQuery = '%' + request.body.searchTerm + '%';
 
     pool.query(
-        'SELECT id, name FROM note_entries WHERE LOWER(name) LIKE LOWER($1) ',
+        'SELECT id, name FROM note_entries WHERE name LIKE ?',
         [
             partialQuery
         ],
@@ -155,7 +99,7 @@ const searchNotes = (request, response) => {
                 // need to handle these in a logger
                 response.status(401).send('Failed to search for notes'); // vague front end error
             } else {
-                response.status(200).send(results.rows);
+                response.status(200).send(results);
             }
         }
     )
